805823496369d24bca268f5c5ca5e4f4451f705e
Carlos Rodrigues <carlos.rodrigues@sinax.com.br>
2017-04-05 14:20:39 -0300

    Hackerrank 30 days of code - Day 0

---
+++ com.hackerrank.carlos/Day0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+
+using namespace std;
+
+int main(){
+    // Print a message on console
+    cout << "Hello, World.\n";
+
+    // Define a variable for receive a text from console
+    string message;
+
+    // Read a text from console
+    cin >> message;
+
+    // Print a message on console
+    cout << message;
+}
