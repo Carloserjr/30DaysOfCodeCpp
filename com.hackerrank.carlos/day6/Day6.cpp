9ec970af496056355fcc6c09e04b6b35f074923e
Carlos Rodrigues <cerodriguesjr@gmail.com>
2017-04-10 23:16:01 -0300

    Solved Let's Review

--- com.hackerrank.carlos/day6/Day6.cpp
+++ com.hackerrank.carlos/day6/Day6.cpp
@@ -1,4 +1,8 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -52,6 +56,34 @@ using namespace std;
  * strings; the first string contains the ordered characters from S's even indices (Rn), and the second string contains
  * the ordered characters from S's odd indices (ak).
  */
-int main(){
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int T;
+    cin >> T;
+    cin.ignore();
+
+    vector<string> results;
+
+    for (int i = 0; i < T; i++) {
+        string S;
+        getline(cin, S);
+        string evens = "";
+        string odds = "";
+
+        for (int j = 0; j < S.size(); ++j) {
+            if (j & 1){
+                odds += S[j];
+            } else {
+                evens += S[j];
+            }
+        }
+
+        results.push_back(evens + " " + odds);
+    }
+
+    for(string text: results){
+        cout << text << endl;
+    }
+
     return 0;
 }
