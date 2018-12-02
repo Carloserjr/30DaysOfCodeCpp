0dfdcb4815689386cd07013570d1251235a2ec50
carlos.rodrigues <cerodriguesjr@gmail.com>
2018-12-02 15:55:48 -0200

    Solved Hello, World.

---
+++ src/com.hackerrank.carlos/day00/Day0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "string"
+
+using namespace std;
+
+int main(){
+    // Print a message on console
+    cout << "Hello, World." << endl;
+
+    // Define a variable to receive the text from console
+    string message;
+
+    // Read a text from console
+    getline(cin, message);
+
+    // Print a message on console
+    cout << message  << endl;
+
+    return 0;
+}
