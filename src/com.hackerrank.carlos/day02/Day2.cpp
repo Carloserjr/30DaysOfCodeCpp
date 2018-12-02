87da27e8e960b9b6d9354471baeb958db442835d
carlos.rodrigues <cerodriguesjr@gmail.com>
2018-12-02 16:07:39 -0200

    Solved Operators.

---
+++ src/com.hackerrank.carlos/day02/Day2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    double mealCost;
+    int tipPercent;
+    int taxPercent;
+
+    cin >> mealCost;
+    cin.ignore();
+
+    cin >> tipPercent;
+    cin.ignore();
+
+    cin >> taxPercent;
+    cin.ignore();
+
+    double totalCost = mealCost + mealCost * (tipPercent/100.0) + mealCost * (taxPercent/100.0);
+
+    // round  to the nearest dollar (integer) and print the result
+    cout << "The total meal cost is " << round(totalCost) << " dollars." << endl;
+    return 0;
+}
